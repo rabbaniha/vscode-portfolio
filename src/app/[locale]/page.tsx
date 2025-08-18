6ef5bc832fb84ea1c4be605f3834ffcc36c50063
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-18 04:35:56 -0400

    fix typo error

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
 
 export default function HomePage() {
-  const t = useTranslations("Home");
+  const t = useTranslations("home");
   return (
     <div>
       <h1>{t("hero.greeting") + " " + t("hero.name")}</h1>
