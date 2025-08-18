c803e7902f72782ffb993eaf8d2ba74a2aafe5fa
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-18 05:42:00 -0400

    feat(Locale switcher) use loacle switcher in home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
+import LocaleSwitcher from "@/components/commom/LocaleSwitcher";
 
 export default function HomePage() {
   const t = useTranslations("home");
   return (
     <div>
       <h1>{t("hero.greeting") + " " + t("hero.name")}</h1>
+      <LocaleSwitcher />
       <Link href="/about">{t("about.title")}</Link>
     </div>
   );
