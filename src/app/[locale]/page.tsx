d45f36faff02e83f33ecb86b52038b6cc825edbf
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-18 04:32:42 -0400

    implement home test page

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,12 @@
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
+
+export default function HomePage() {
+  const t = useTranslations("Home");
+  return (
+    <div>
+      <h1>{t("hero.greeting") + " " + t("hero.name")}</h1>
+      <Link href="/about">{t("about.title")}</Link>
+    </div>
+  );
+}
