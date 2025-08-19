2b71c8101bf9d471f739905a9c824e3919aa0485
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-19 01:18:35 -0400

    style(animate) add animate for modern navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,22 @@
+"use client";
 import { Link } from "@/i18n/navigation";
 import { useTranslations } from "next-intl";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { ThemeSwitcher } from "../commom/ThemeSwitcher";
 import LocaleSwitcher from "../commom/LocaleSwitcher";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
   const t = useTranslations("navigation");
+  const pathname = usePathname();
+  const cleanPathname = pathname.replace(/^\/(en|fa|ar)/, "");
+  const previousPath = useRef<string | null>(null);
+
+  useEffect(() => {
+    // مقدار قبلی رو ذخیره می‌کنیم
+    previousPath.current = cleanPathname;
+  }, [cleanPathname]);
+
   return (
     <section className=" w-full border-b flex items-center justify-between bg-transparent">
       <div className="flex items-center bg-transparent">
@@ -13,21 +24,73 @@ const Header = () => {
           {t("name")}
         </div>
 
-        <ul className=" flex items-center justify-center">
-          <Link href={"/"}>
+        <ul className=" flex items-center justify-center ">
+          <Link href={"/"} className=" relative">
             <li className=" border-r px-7 py-4">{t("home")}</li>
+            <div
+              className={`absolute h-2 bottom-0 right-0 left-0 bg-accent  ${
+                cleanPathname === "" ? "visible" : "hidden"
+              } ${
+                previousPath.current === "/about"
+                  ? "animate-entry-right-100"
+                  : previousPath.current === "/project"
+                  ? "animate-entry-right-200"
+                  : previousPath.current === "/contact"
+                  ? "animate-entry-right-300"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
-          <Link href={"/about"}>
+          <Link href={"/about"} className=" relative">
             <li className=" border-r px-7 py-4">{t("about")}</li>
+            <div
+              className={`absolute h-2 bottom-0 right-0 left-0 bg-accent  ${
+                cleanPathname === "/about" ? "visible" : "hidden"
+              } ${
+                previousPath.current === ""
+                  ? "animate-entry-left-100"
+                  : previousPath.current === "/project"
+                  ? "animate-entry-right-100"
+                  : previousPath.current === "/contact"
+                  ? "animate-entry-right-200"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
-          <Link href={"/project"}>
+          <Link href={"/project"} className=" relative">
             <li className=" border-r px-7 py-4">{t("project")}</li>
+            <div
+              className={`absolute h-2 bottom-0 right-0 left-0 bg-accent ${
+                cleanPathname === "/project" ? "visible" : "hidden"
+              }  ${
+                previousPath.current === ""
+                  ? "animate-entry-left-200"
+                  : previousPath.current === "/about"
+                  ? "animate-entry-left-100"
+                  : previousPath.current === "/contact"
+                  ? "animate-entry-right-100"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
-          <Link href={"contact"}>
+          <Link href={"contact"} className=" relative">
             <li className=" border-r px-7 py-4">{t("contact")}</li>
+            <div
+              className={`absolute h-2 bottom-0 right-0 left-0 bg-accent ${
+                cleanPathname === "/contact" ? "visible" : "hidden"
+              } ${
+                previousPath.current === ""
+                  ? "animate-entry-left-300"
+                  : previousPath.current === "/about"
+                  ? "animate-entry-left-200"
+                  : previousPath.current === "/project"
+                  ? "animate-entry-left-100"
+                  : ""
+              }`}
+            ></div>
           </Link>
         </ul>
       </div>
