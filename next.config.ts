d4af0f5f1b1992602eb39fc8f68c849398c7c1bb
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-28 11:53:11 -0400

    add mui icons to project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js", // This tells Turbopack to treat the SVG as a JavaScript module
+      },
+    },
+  },
+};
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
