9e1aa736b1945eaa2e8bfdf12ef63e785ed1ab5c
rabbaniha <heydarrabbaniha@gmail.com>
2025-08-31 03:54:53 -0400

    fix height of content component and space between number lines and stars

--- src/components/about/desktop/DesktopContent.tsx
+++ src/components/about/desktop/DesktopContent.tsx
@@ -1,19 +1,26 @@
 "use client";
+import React from "react";
 import { useContentTabStore } from "@/stores";
-import { ActiveContentTabType } from "@/types";
-import React, { Dispatch, SetStateAction } from "react";
+import { useTranslations } from "next-intl";
+import CodeLikeText from "@/components/commom/CodeLikeText";
 
 const DesktopContent = () => {
   const { activeContentTab } = useContentTabStore();
+  const t = useTranslations("about");
+
   return (
-    <div className=" p-8 w-full h-full flex items-start justify-start bg-green-800">
+    <div className="p-8 w-full flex-1 min-h-0 flex items-start justify-start bg-gray-900">
       {activeContentTab === null ? (
-        <div className=" w-full h-full flex items-center justify-center">
+        <div className="w-full h-full flex items-center justify-center">
           No content to show
         </div>
       ) : (
         <>
-          {activeContentTab === "bio" && <div>Bio</div>}
+          {activeContentTab === "bio" && (
+            <div className="w-full h-[60vh] overflow-y-auto space-y-2">
+              <CodeLikeText text={t("personal.bio.description")} />
+            </div>
+          )}
           {activeContentTab === "interests" && <div>Interests</div>}
           {activeContentTab === "education" && <div>Education</div>}
         </>
