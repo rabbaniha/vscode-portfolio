bb6446dacdb7fd59461e412e68f1d5f8cdaec035
rabbaniha <heydarrabbaniha@gmail.com>
2025-09-02 03:46:43 -0400

    feat(open tabs folder) add new prop "openTabsFolder" to tabs stor

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,13 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { ContentTabStoreType } from "@/types";
+import { ActiveContentTabType, ContentTabStoreType } from "@/types";
 
 export const useContentTabStore = create<ContentTabStoreType>()(
   persist(
     (set) => ({
-      activeContentTab: null,
-      openTabs: [],
+      activeContentTab: "bio",
+      openTabs: ["bio"],
+      openTabsForFolders: ["bio"],
       setActiveContentTab: (tab) =>
         set((state) => {
           if (!tab) return { activeContentTab: null };
@@ -15,6 +16,9 @@ export const useContentTabStore = create<ContentTabStoreType>()(
             openTabs: state.openTabs.includes(tab)
               ? state.openTabs
               : [...state.openTabs, tab],
+            openTabsForFolders: state.openTabsForFolders.includes(tab)
+              ? state.openTabsForFolders
+              : [...state.openTabsForFolders, tab],
           };
         }),
       closeTab: (tab) =>
@@ -22,13 +26,17 @@ export const useContentTabStore = create<ContentTabStoreType>()(
           const newTabs = state.openTabs.filter((t) => t !== tab);
           return {
             openTabs: newTabs,
+            openTabsForFolders: newTabs,
             activeContentTab:
               state.activeContentTab === tab
                 ? newTabs.at(-1) ?? null
                 : state.activeContentTab,
           };
         }),
+      updateOpenTabs: (tabs) =>
+        set({ openTabsForFolders: tabs as ActiveContentTabType[] }),
     }),
+
     { name: "content-tabs-storage" }
   )
 );
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,8 @@ export type ActiveContentTabType = "bio" | "interests" | "education" | null;
 export type ContentTabStoreType = {
   activeContentTab: ActiveContentTabType;
   openTabs: ActiveContentTabType[];
+  openTabsForFolders: ActiveContentTabType[];
   setActiveContentTab: (tab: ActiveContentTabType) => void;
   closeTab: (tab: ActiveContentTabType) => void;
+  updateOpenTabs: (tabs: string[]) => void;
 };
