5b2d916f9bc200b619076c35176fac6354f5e8b6
rabbaniha <heydarrabbaniha@gmail.com>
2025-09-03 12:06:24 -0400

    create data file for store skills content

---
+++ src/data/skills.ts
@@ -0,0 +1,60 @@
+import Expressjs from "@/icons/Expressjs";
+import Nextjs from "@/icons/Nextjs";
+import Nodejs from "@/icons/Nodejs";
+import Reactjs from "@/icons/Reactjs";
+import HTML from "@/icons/Html";
+import { Skill } from "@/types";
+import Css from "@/icons/Css";
+import Docker from "@/icons/Docker";
+import Mongodb from "@/icons/Mongodb";
+import Sql from "@/icons/Sql";
+import Tailwindcss from "@/icons/Tailwindcss";
+import Linux from "@/icons/Linux";
+
+export const skillsList: Skill[] = [
+  {
+    name: "Node Js",
+    icon: Nodejs,
+  },
+  {
+    name: "Express Js",
+    icon: Expressjs,
+  },
+  {
+    name: "React Js",
+    icon: Reactjs,
+  },
+  {
+    name: "Next Js",
+    icon: Nextjs,
+  },
+  {
+    name: "HTML5",
+    icon: HTML,
+  },
+  {
+    name: "CSS3",
+    icon: Css,
+  },
+  {
+    name: "Tailwindcss",
+    icon: Tailwindcss,
+  },
+
+  {
+    name: "Mongodb",
+    icon: Mongodb,
+  },
+  {
+    name: "Sql",
+    icon: Sql,
+  },
+  {
+    name: "Linux",
+    icon: Linux,
+  },
+  {
+    name: "Docker",
+    icon: Docker,
+  },
+];
