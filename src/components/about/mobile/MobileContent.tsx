a9f4e71ad77335c8a2433ba977fce88d1278e834
rabbaniha <heydarrabbaniha@gmail.com>
2025-09-07 02:09:51 -0400

    change show content in mobile about page and make fixed content

--- src/components/about/mobile/MobileContent.tsx
+++ src/components/about/mobile/MobileContent.tsx
@@ -1,25 +1,57 @@
 "use client";
-import { useContentTabStore } from "@/stores";
-import { useTranslations } from "next-intl";
 import React from "react";
+import { useContentTabStore } from "@/stores";
+import { X } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { easeOut, motion, AnimatePresence } from "framer-motion";
 
 const MobileContent = () => {
   const t = useTranslations("about");
-  const { activeContentTab } = useContentTabStore();
+  const { activeContentTab, setActiveContentTab } = useContentTabStore();
   const parentTab =
     activeContentTab === "bio" ||
     activeContentTab === "education" ||
     activeContentTab === "interests"
       ? "personal"
       : activeContentTab === "experienc" || activeContentTab === "skills"
-      ? "prefessional"
+      ? "professional"
       : "hobbies";
+
+  const itemVariants = {
+    hidden: {
+      opacity: 0.7,
+      y: "100%",
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: easeOut,
+      },
+    },
+  };
+
   return (
-    <div className=" p-8 overflow-y-auto scrollbar-hide flex-1">
-      <p className=" text-justify">
-        {t(`${parentTab}.${activeContentTab}.description`)}
-      </p>
-    </div>
+    <AnimatePresence>
+      {activeContentTab !== null && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={itemVariants}
+          className=" fixed bg-background text-foreground inset-0 top-5  w-[95%] mx-auto h-[95vh] z-[999]  rounded-sm border-border p-8 overflow-y-auto scrollbar-hide"
+        >
+          <X
+            className=" size-10 cursor-pointer transition-all absolute top-0 left-0 p-2  hover:text-destructive"
+            onClick={() => setActiveContentTab(null)}
+          />
+          <p className=" text-justify">
+            {t(`${parentTab}.${activeContentTab}.description`)}
+          </p>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
